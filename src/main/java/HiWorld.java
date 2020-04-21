46d47be96983a34664d5373f6e973e5e408f1a5c
sxw220 <sw15137394047@163.com>
2020-04-21 14:19:42 +0800

    test

---
+++ src/main/java/HiWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @Author 孙新伟
+ * @Date 2020/4/21 14:16
+ * @Version 1.0
+ */
+public class HiWorld {
+    public static void main(String args[]){
+        System.out.println("Hi World");
+    }
+}
